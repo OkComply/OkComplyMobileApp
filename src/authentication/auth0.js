95dd965a813505b13f68100aaff731bb79c74582
Raeef Ibrahim <raeef.ibrahim@hva.nl>
2020-04-29 21:33:53 +0200

    Json Nontification

---
+++ src/authentication/auth0.js
@@ -0,0 +1,11 @@
+import Auth0 from 'react-native-auth0';
+import { auth } from 'firebase';
+
+const auth0 = new Auth0({
+  domain: 'okapi-prod.eu.auth0.com',
+  clientId: 'Z977z6OWUxi58x41rndANbZIy49o3iKR',
+});
+
+
+
+export default auth0
